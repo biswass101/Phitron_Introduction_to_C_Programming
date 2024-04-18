44fddc029b4d5da7e3cceb810b69d59a160298bb
biswass101 <biswassnaeemcse@gmail.com>
2024-04-18 20:22:36 +0600

    assignment 2 added

---
+++ Moudule-8(Assignment)/sayit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n; scanf("%d", &n);
+    for(int i = 0; i < n; i++) printf("%d. I Want More Assignments\n", i + 1);
+    return 0;
+}
