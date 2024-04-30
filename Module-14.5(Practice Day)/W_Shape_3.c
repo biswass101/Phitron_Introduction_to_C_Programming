94b8e932b126ff03e4fd28b7a67463ff548a27fc
biswass101 <biswassnaeemcse@gmail.com>
2024-04-30 21:59:35 +0600

    func_and_array_practice_day

---
+++ Module-14.5(Practice Day)/W_Shape_3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n; scanf("%d", &n);
+    int k = 1; int s = n -1;
+    for(int i = 1; i <= 2*n; i++)
+    {
+        for(int j = 1; j <= s; j++) printf(" ");
+        for(int  j = 1; j <= k; j++) printf("*");
+        printf("\n");
+        if(i == n) continue;
+        else if(i <= n - 1)
+        {
+            k += 2;
+            s--;
+        }
+        else
+        {
+            k -= 2;
+            s++;
+        }
+        
+    }
+    return 0;
+}
